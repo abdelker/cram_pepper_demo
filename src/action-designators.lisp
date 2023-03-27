f28ffcee03f242c7358ad386aebe1cd43fc064d7
monaAK <34164926+abdelker@users.noreply.github.com>
2023-03-27 14:08:10 +0200

    add other action desig

--- src/action-designators.lisp
+++ src/action-designators.lisp
@@ -7,4 +7,19 @@
       (desig-prop ?desig (:type  :looking))
       (desig-prop ?desig (:target ?object-desig))
       ;;(desig-prop ?desig (:arm ?arm))
-      ) )
+      ) 
+;;moving-to   
+  (<- (desig:action-grounding ?desig (move-to ?desig))
+      (desig-prop ?desig (:type  :moving-to))
+      (desig-prop ?desig (:to ?place)))
+
+;;saying
+ (<- (desig:action-grounding ?desig (say ?desig))
+      (desig-prop ?desig (:type  :saying))
+      (desig-prop ?desig (:sentence ?sentence)))
+
+;;scannin
+  (<- (desig:action-grounding ?desig (scan ?desig))
+      (desig-prop ?desig (:type  :scanning))
+      (desig-prop ?desig (:zone ?zone)))
+      ) 
--- src/motion-designators.lisp
+++ src/motion-designators.lisp
@@ -7,9 +7,49 @@
  ;; arm
 )
 
+(defstruct pepper-moving-motion
+  "Represents a moving motion"
+  x
+  y
+)
+
+(defstruct pepper-scanning-motion
+  "Represents a moving motion"
+  x
+  y
+  z
+)
+
 (def-fact-group pepper-navigation-motion-designators (motion-grounding)
 (<- (desig:motion-grounding ?desig (look ?motion))
     (desig-prop ?desig (:type :looking))
     (desig-prop ?desig (:target ?target))
     ;;(desig-prop ?desig (:arm ?arm))
-    (lisp-fun make-pepper-looking-motion :target ?target ?motion)))
+    (lisp-fun make-pepper-looking-motion :target ?target ?motion))
+
+(<- (desig:motion-grounding ?desig (move-to ?motion))
+    (desig-prop ?desig (:type :moving))
+    (desig-prop ?desig (:x ?x))
+    (desig-prop ?desig (:y ?y))
+    (lisp-fun make-avatar-motion :x ?x :y ?y ?motion))
+    
+(<- (desig:motion-grounding ?desig (scan ?motion))
+    (desig-prop ?desig (:type :scanning))
+    (desig-prop ?desig (:x ?x))
+    (desig-prop ?desig (:y ?y))
+    (desig-prop ?desig (:z ?z))
+    (lisp-fun make-avatar-motion :x ?x :y ?y :z ?z ?motion))    
+    )
+
+(defstruct pepper-communication-motion
+  "Represents a moving motion"
+  sentence
+)
+
+(def-fact-group pepper-communication-motion-designators (motion-grounding)  
+  (<- (desig:motion-grounding ?desig (say ?motion))
+      (desig-prop ?desig (:type :saying))
+      (desig-prop ?desig (:sentence ?sentence))
+      (lisp-fun make-avatar-motion :sentence ?sentence ?motion)
+      ))
+  
