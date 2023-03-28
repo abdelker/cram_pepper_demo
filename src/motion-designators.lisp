d86d4eb743164f8da025577327340f767cee7831
monaAK <34164926+abdelker@users.noreply.github.com>
2023-03-28 13:02:41 +0200

    add communication motion desig

--- src/motion-designators.lisp
+++ src/motion-designators.lisp
@@ -9,8 +9,8 @@
 
 (defstruct pepper-moving-motion
   "Represents a moving motion"
-  x
-  y
+  x-val
+  y-val
 )
 
 (defstruct pepper-scanning-motion
@@ -29,27 +29,27 @@
 
 (<- (desig:motion-grounding ?desig (move-to ?motion))
     (desig-prop ?desig (:type :moving))
-    (desig-prop ?desig (:x ?x))
-    (desig-prop ?desig (:y ?y))
-    (lisp-fun make-avatar-motion :x ?x :y ?y ?motion))
+    (desig-prop ?desig (:x-val ?x-val))
+    (desig-prop ?desig (:y-val ?y-val))
+    (lisp-fun make-pepper-moving-motion :x-val ?x-val :y-val ?y-val ?motion))
     
 (<- (desig:motion-grounding ?desig (scan ?motion))
     (desig-prop ?desig (:type :scanning))
     (desig-prop ?desig (:x ?x))
     (desig-prop ?desig (:y ?y))
     (desig-prop ?desig (:z ?z))
-    (lisp-fun make-avatar-motion :x ?x :y ?y :z ?z ?motion))    
+    (lisp-fun make-pepper-scanning-motion :x ?x :y ?y :z ?z ?motion))    
     )
 
 (defstruct pepper-communication-motion
-  "Represents a moving motion"
+  "Represents a speaking motion"
   sentence
 )
 
 (def-fact-group pepper-communication-motion-designators (motion-grounding)  
-  (<- (desig:motion-grounding ?desig (say ?motion))
-      (desig-prop ?desig (:type :saying))
+  (<- (desig:motion-grounding ?desig (speak ?motion))
+      (desig-prop ?desig (:type :speaking))
       (desig-prop ?desig (:sentence ?sentence))
-      (lisp-fun make-avatar-motion :sentence ?sentence ?motion)
+      (lisp-fun make-pepper-communication-motion :sentence ?sentence ?motion)
       ))
   
