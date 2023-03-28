804d0e9451d53c8922f2aa1f060df5801e57572c
monaAK <34164926+abdelker@users.noreply.github.com>
2023-03-28 13:01:15 +0200

    add pm for communication

--- src/process-modules.lisp
+++ src/process-modules.lisp
@@ -1,24 +1,43 @@
 (in-package :pepper-demo)
 
+;;navigation
 (def-process-module pepper-navigation (motion-designator)
   (roslisp:ros-info (pepper-process-modules)
                     "pepper navigation invoked with motion designator `~a'."
                     motion-designator)
   (destructuring-bind (command motion) (reference motion-designator)
     (ecase command
-(look
-        ( let (
-               (target (pepper-looking-motion-target motion))
-              ;; (arm (pepper-looking-motion-arm motion))
-               )
-         (let ((my-target-type (desig:desig-prop-value target :type)))
+     (look
+             ( let (
+                    (target (pepper-looking-motion-target motion)))
+                   ;; (arm (pepper-looking-motion-arm motion))
+               
+              (let ((my-target-type (desig:desig-prop-value target :type)))
 
-          (cond 
-          ((string= my-target-type
-                :object)
+               (cond 
+                ((string= my-target-type
+                      :object)
 
-          (let ((obj (desig:desig-prop-value target :name)))
-             (print obj)
-             (look-object obj))))
+                 (let ((obj (desig:desig-prop-value target :name)))
+                    (print obj)
+                    (look-object obj)))))))
              
-             ))))))
+             
+             
+     (move-to
+            (let ((x  (pepper-moving-motion-x-val  motion))
+                  (y  (pepper-moving-motion-y-val  motion)))
+              (call-move-to-srv x y))))))            
+             
+  ;;commmunication           
+(def-process-module pepper-communication (motion-designator)
+ (roslisp:ros-info (pepper-process-modules)
+                   "pepper navigation invoked with motion designator `~a'."
+                   motion-designator)
+ (destructuring-bind (command motion) (reference motion-designator)
+   (ecase command         
+    (speak
+            
+           (let ((sentence  (pepper-communication-motion-sentence motion)))
+            (print sentence)
+            (call-say-srv sentence))))))                  
