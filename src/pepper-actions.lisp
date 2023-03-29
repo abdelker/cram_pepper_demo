402a7b6711dc5ffb0b5f294e7d0200c949a32186
monaAK <34164926+abdelker@users.noreply.github.com>
2023-03-29 16:09:06 +0200

    change variable name for lexical binding

--- src/pepper-actions.lisp
+++ src/pepper-actions.lisp
@@ -1,7 +1,7 @@
 (in-package :pepper-demo)
 
-;;(defvar *buffer-list* nil "Buffer List")
-;;(defvar *value-list* nil "Buffer List")
+;;(defvar buffer-list nil "Buffer List")
+;;(defvar value-list nil "Buffer List")
 
 ;;PRIORITIZE 3
 ;;INHIBIT -1
@@ -20,26 +20,26 @@
     (sleep sleep-time))))
 
 (defun look-at (&optional (buffer-name nil))
-  (let ((*buffer-list* (vector  "speaking" "env_monitoring" "human_monitoring")))
+  (let ((buffer-list (vector  "speaking" "env_monitoring" "human_monitoring")))
  ;;(look-at "env_monitoring")
     (cond 
     ((string= buffer-name "speaking")  
                 
       (progn 
-        (let ((*value-list* (vector  3 -1 0)))
-        (send-prio-info *buffer-list* *value-list*)
+        (let ((value-list (vector  3 -1 0)))
+        (send-prio-info buffer-list value-list)
         ))) 
     
     ((string= buffer-name "env_monitoring")  
                 
       (progn 
-        (let ((*value-list* (vector  -1 3 0)))
-        (send-prio-info *buffer-list* *value-list*)
+        (let ((value-list (vector  -1 3 0)))
+        (send-prio-info buffer-list value-list)
         ))) 
 
     ((progn 
-        (let ((*value-list* (vector  -1 -1 0)))
-        (send-prio-info *buffer-list* *value-list*)
+        (let ((value-list (vector  -1 -1 0)))
+        (send-prio-info buffer-list value-list)
         )))        
    ;; ((princ "try again"))
     )
@@ -52,12 +52,12 @@
     ((string= selected-arm nil)  
                 
       (progn 
-        (let (((*buffer-list* (vector  "social" "manipulation")))
-        ((*value-list* (vector  -1 3))))
-        (send-prio-arm-right-info *buffer-list* *value-list*)
-        (send-prio-arm-left-info *buffer-list* *value-list*)
+        (let (((buffer-list (vector  "social" "manipulation")))
+        ((value-list (vector  -1 3))))
+        (send-prio-arm-right-info buffer-list value-list)
+        (send-prio-arm-left-info buffer-list value-list)
         ))) 
-  ;; ((let (((*buffer-list* (vector  "social" "manipulation")))
+  ;; ((let (((buffer-list (vector  "social" "manipulation")))
 
     )) |#
     
